feaaa0dde348f5b32712427a5edba8403884eb66
Mudev <muaykillz2548@gmail.com>
2024-06-14 11:12:29 +0700

    🚧 build(app): Built docker and deploy to google cloud run

--- ai-server/run.py
+++ ai-server/run.py
@@ -2,5 +2,5 @@ import uvicorn
 from os import getenv
 
 if __name__ == '__main__':
-    port = int(getenv("PORT", 8000))
-    uvicorn.run("app.main:app", port=port, reload=True)
+    port = int(getenv("PORT", 8080))  # ใช้พอร์ต 8080 ที่ตั้งค่าโดยระบบ
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
